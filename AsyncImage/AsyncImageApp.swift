251e971a2192e416332129b93470ab43c0343d24
kittyboard <76805188+kittyboard@users.noreply.github.com>
2023-07-18 09:47:28 +0900

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by 통이 on 2023/07/18.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
